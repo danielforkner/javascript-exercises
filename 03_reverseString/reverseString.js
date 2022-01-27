76f56a996ec6851a756c4d31a24b4040128653ec
danielforkner <daniel.forkner@gmail.com>
2022-01-27 13:21:52 -0500

    completed 03 and 04

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let arr = string.split('');
+    newStr = arr.reverse().join('');
+    return newStr;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+    for (num of args) {
+        let index = array.indexOf(num);
+        if (index !== -1) {
+            array.splice(index, 1);
+        }
+    };
+    return array;
 };
+
+removeFromArray([1, 2, 3], 1, 2, 3)
 
 // Do not edit below this line
 module.exports = removeFromArray;
