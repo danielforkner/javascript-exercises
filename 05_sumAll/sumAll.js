60ceffd53df96fef6ff3d0236608765ca786529b
danielforkner <daniel.forkner@gmail.com>
2022-01-27 21:52:01 -0500

    completed up to 07

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    if (num1 < 0 || num2 < 0) {return 'ERROR'};
+    if (typeof num1 != 'number' || typeof num2 != 'number') {return 'ERROR'};
+    
+    let sum = num2 > num1 ? num1 : num2;
+    let low = num2 > num1 ? num1 : num2;
+    let high = num2 > num1 ? num2 : num1;
+    while (low < high) {
+        low++;
+        sum += low;
+    }
+    return sum;
 };
 
 // Do not edit below this line
