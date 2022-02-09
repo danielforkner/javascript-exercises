2418a93ccda4b19730be7bdf97e5721b8a55af4d
danielforkner <daniel.forkner@gmail.com>
2022-02-08 21:33:48 -0500

    getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,28 @@
-const getTheTitles = function() {
+// help from https://stackoverflow.com/questions/64635476/can-i-use-filter-to-extract-values-from-an-array-of-objects
 
+const getTheTitles = function(books) {
+    return books
+        .filter(book => book.title)
+        .map(book => book.title)
 };
+
+
+let array = [
+     {
+      "author": "Name",
+      "title": "Book",
+    },
+     {
+      "author": "Name2",
+      "title": "Book2",
+    },
+     {
+      "author": "Name3",
+    },
+      ];
+
+console.log(getTheTitles(array));
+
 
 // Do not edit below this line
 module.exports = getTheTitles;
