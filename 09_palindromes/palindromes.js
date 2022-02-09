32534223167af8230fc468b6a5d2e974ae9acf17
danielforkner <daniel.forkner@gmail.com>
2022-02-08 19:20:00 -0500

    palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,24 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let strArr = str.toUpperCase().split('');
+    let left = 0;
+    let right = strArr.length - 1;
+    while (left < right) {
+        // ignore anything not a char
+        if (strArr[left].charCodeAt(0) < 65 || strArr[left].charCodeAt(0) > 90) {
+            left++;
+        } else if (strArr[right].charCodeAt(0) < 65 || strArr[right].charCodeAt(0) > 90) {
+            right--;
+        } else if (strArr[left] === strArr[right]) {
+            left++;
+            right--;
+        } else {
+            return false;
+        }
+    }
+    return true;
 };
+
+console.log(palindromes('rac!ecaR!'));
 
 // Do not edit below this line
 module.exports = palindromes;
